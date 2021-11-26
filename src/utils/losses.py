2154ee65429cf932f61fc65cac742412de389780
Sansiddh Jain <sansiddh@wadhwaniai.org>
2021-11-26 15:22:35 +0530

    Added module for all losses

---
+++ src/utils/losses.py
@@ -0,0 +1,8 @@
+from segmentation_models_pytorch.losses import *
+from torch.nn import (BCELoss, BCEWithLogitsLoss, CosineEmbeddingLoss,
+                      CrossEntropyLoss, CTCLoss, GaussianNLLLoss,
+                      HingeEmbeddingLoss, HuberLoss, KLDivLoss, L1Loss,
+                      MarginRankingLoss, MSELoss, MultiLabelMarginLoss,
+                      MultiLabelSoftMarginLoss, MultiMarginLoss, NLLLoss,
+                      PoissonNLLLoss, SmoothL1Loss, SoftMarginLoss,
+                      TripletMarginLoss, TripletMarginWithDistanceLoss)
