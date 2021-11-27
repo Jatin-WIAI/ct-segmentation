c0de090ab4e151d01aa88996d1da43b3f4afaa15
Sansiddh Jain <sansiddh@wadhwaniai.org>
2021-11-27 22:12:29 +0530

    Refactored abdomenct1k

--- src/data/datasets/abdomenct1k.py
+++ src/data/datasets/abdomenct1k.py
@@ -1,6 +1,7 @@
 import os
 import random
 import time
+from glob import glob
 
 import numpy as np
 import tqdm
@@ -25,12 +26,10 @@ class abdomenCT1k(Dataset):
         ROOT='/scratche/users/sansiddh/abdomenCT-1k/',
         transform=None,
         phase="train",
-        task="semantic-segmentation",
-        feature=None,
         sample=None,
         fraction=1.0,
         split_version="v3",
-        NUM_CLASSES=2,
+        num_classes=2,
         **kwargs,
     ):
 
@@ -45,30 +44,23 @@ class abdomenCT1k(Dataset):
         }
 
         # Loads images list
-        self.splitfile_path = os.path.join(
-            self.ROOT, "splits", split_version, f"{phase}.txt"
-        )
+        if split_version is not None:
+            self.splitfile_path = os.path.join(
+                self.ROOT, "splits", split_version, f"{phase}.txt"
+            )
+        else:
+            self.splitfile_path = None
+        
+        # Gets images from splitfile path. If splitfile path is None, loads all images
         self.image_list = self.get_image_list(self.splitfile_path)
 
         # Filter to a fraction of the data for dry-runs
-        self.image_list = self.get_dataset_subset(fraction)
+        if fraction != 1:
+            self.image_list = self.get_dataset_subset(fraction)
+        print("> No. total samples using: {}".format(len(self.image_list)))
 
         # Number of classes for classification
-        self.num_classes = NUM_CLASSES
-
-        # Create labels
-        print(f"Creating labels for task {task}")
-        if feature is not None:
-            assert (
-                feature in self.feature_code_to_name_mapping
-            ), "FS2: Unknown feature index for code-to-name mapping"
-            print(f"Feature : {self.feature_code_to_name_mapping[feature]}")
-        self.image2label_dict = self.create_labels(
-            self.image_list, task, feature)
-
-        # Get positive and negative indices (for binary class task)
-        classwise_indices = self.split_by_class_labels()
-        self.neg_indices, self.pos_indices = classwise_indices[0], classwise_indices[1]
+        self.num_classes = num_classes
 
         if phase == "train":
             # TODO: Should this disabled by default?
@@ -86,78 +78,19 @@ class abdomenCT1k(Dataset):
         image.close()
         return img
 
-    def create_labels(self, image_list, task, feature):
-        # ROUND2-TODO: Complex function, deserving splitting into parts. Could also be part of the abstraction
-        start_time = time.time()
-        target_dict = {}
-        for _, imagepath in enumerate(tqdm.tqdm(image_list, desc="Creating labels")):
-            if task == "disease-classification":
-                # TODO: Assumes specific folder structure. May break if that's changed
-                # ROUND2-TODO: put "template" paths in utils.constants.
-                if imagepath.split("/")[5] == "positive":
-                    target_dict[imagepath] = 1
-                else:
-                    target_dict[imagepath] = 0
-            elif task == "feature-classification":
-                annot_path = imagepath.replace(".jpg", ".txt")
-                with open(annot_path, "r") as f:
-                    annotations = f.read().split("\n")
-                if annotations == [""]:
-                    target_dict[imagepath] = 0
-                else:
-                    for j in range(len(annotations) - 1):
-                        feature_code = int(annotations[j][0])
-                        if feature == feature_code:
-                            target_dict[imagepath] = 1
-                        else:
-                            target_dict[imagepath] = 0
-            elif task == "feature-detection":
-                target_dict[imagepath] = {}
-                annot_path = imagepath.replace(".jpg", ".txt")
-                with open(annot_path, "r") as f:
-                    annotations = f.read().split("\n")
-                if annotations == [""]:
-                    target_dict[imagepath] = 0
-                else:
-                    import pdb
-
-                    pdb.set_trace()
-            else:
-                raise ValueError(f"task {task} is unrecognised.")
-
-        print(
-            "Total time to create the labels for task {} : {:.4f}".format(
-                task, time.time() - start_time
-            )
-        )
-        return target_dict
-
     def get_image_list(self, splitfile_path):
         """Read images from splits file and shuffle
         """
-        with open(splitfile_path, "r") as f:
-            image_list = f.read().split("\n")
+        if splitfile_path is not None:
+            with open(splitfile_path, "r") as f:
+                image_list = f.read().split("\n")
+        else:
+            image_list = glob(f'{self.ROOT}Images/*/*.jpg')
 
         random.shuffle(image_list)
         print("> Found total images : {}".format(len(image_list)))
 
         return image_list
-
-    def split_by_class_labels(self):
-        classwise_indices = [[] for i in range(self.num_classes)]
-
-        for i, image in enumerate(self.image_list):
-            class_label = self.image2label_dict[image]
-            classwise_indices[class_label].append(i)
-
-        for class_label in range(self.num_classes):
-            print(
-                "> No. of samples for class {}: {}".format(
-                    class_label, len(classwise_indices[class_label])
-                )
-            )
-
-        return classwise_indices
 
     def downsampling_negatives(self):
         """Assumes negatives are more than positives, and downsamples thme
@@ -190,23 +123,26 @@ class abdomenCT1k(Dataset):
         return upsampled_image_list
 
     def get_dataset_subset(self, fraction):
-        if fraction != 1.0:
-            num_samples = int(fraction * len(self.image_list))
-            # ROUND2-TODO: consider a random.shuffle(mylist) followed by sub selection mylist[:self.pos_indices].
-            image_list = random.sample(self.image_list, num_samples)
+        num_samples = int(fraction * len(self.image_list))
+        # ROUND2-TODO: consider a random.shuffle(mylist) followed by sub selection mylist[:self.pos_indices].
+        # SJ : This is already happening, we do random.shuffle(list) when we load the images
+        image_list = random.sample(self.image_list, num_samples)
 
-            print("> No. total samples using: {}".format(len(image_list)))
-            return image_list
-        else:
-            print("> No. total samples using: {}".format(len(self.image_list)))
-            return self.image_list
+        print("> No. total samples using: {}".format(len(image_list)))
+        return image_list
 
     def __len__(self):
         return len(self.image_list)
 
     def __getitem__(self, idx):
-        image = self.load_and_process_image(self.image_list[idx], self.transform)
-        label = self.image2label_dict[self.image_list[idx]]
         image_path = self.image_list[idx]
+        mask_path = image_path.replace('.jpg', '.npy').replace('Images', 'Masks')
+        image = self.load_and_process_image(image_path, self.transform)
+        mask = np.load(mask_path)
 
-        return (image, label, image_path)
+        target = {
+            'image': image,
+            'mask': mask
+        }
+
+        return target
