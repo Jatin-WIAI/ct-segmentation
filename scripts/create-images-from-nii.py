c0e4ae3303f7afab18891095ec19941ab6d24242
Sansiddh Jain <sansiddh@wadhwaniai.org>
2022-01-10 21:00:34 +0530

    Scripts for converting .nii.gz to jpg

--- scripts/create-images-from-nii.py
+++ scripts/create-images-from-nii.py
@@ -1,3 +1,8 @@
+"""This script converts the .nii.gz CT scans of the abdomenCT-1k dataset into grayscale PNG images
+It is assumed that the windowing is set to (-300, 300). 
+The grayscale images are stored as standard 3 channel RGB images
+"""
+
 import os
 import numpy as np
 import nibabel as nib
@@ -9,7 +14,7 @@ from os.path import basename
 
 
 ROOT_DIR = '/scratche/users/sansiddh/abdomenCT-1k/'
-WINDOWING_LEN = 300
+WINDOWING_LIMITS = (-300, 300)
 all_masks = glob(ROOT_DIR+'Masks/*.nii.gz')
 len(all_masks)
 
@@ -20,12 +25,12 @@ for mask in all_masks:
     ctscan = nib.load(ctscan_path).get_fdata()
     for i in range(ctscan.shape[2]):
         ctscan_img = ctscan[:, :, i]
-        ctscan_img[ctscan_img > WINDOWING_LEN] = WINDOWING_LEN
-        ctscan_img[ctscan_img < -WINDOWING_LEN] = -WINDOWING_LEN
+        ctscan_img[ctscan_img > WINDOWING_LIMITS[1]] = WINDOWING_LIMITS[1]
+        ctscan_img[ctscan_img < WINDOWING_LIMITS[0]] = WINDOWING_LIMITS[0]
         hounsfield_mask = np.rot90(ctscan_img, k=1)
 
-        gsimg_channel = ((hounsfield_mask + WINDOWING_LEN) *
-                         255/(2*WINDOWING_LEN)).astype(np.uint8)
+        gsimg_channel = ((hounsfield_mask - WINDOWING_LIMITS[0]) *
+                         255/(WINDOWING_LIMITS[1] - WINDOWING_LIMITS[0])).astype(np.uint8)
         img = np.repeat(gsimg_channel[:, :, np.newaxis], 3, axis=2)
 
         img = Image.fromarray(img)
