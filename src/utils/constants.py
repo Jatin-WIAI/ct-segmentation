8228c2c4925591e4274476bd08080fc607fda37c
Sansiddh Jain <sansiddh@wadhwaniai.org>
2022-01-10 21:05:00 +0530

    Replace config with configs

--- src/utils/constants.py
+++ src/utils/constants.py
@@ -5,6 +5,6 @@ from os.path import dirname, join, realpath
 # Path to the ultrasound repository. Go up 3 directories: ROOT_DIR/src/utils/constants.py
 REPO_PATH = dirname(dirname(dirname(realpath(__file__))))
 
-CONFIG_DIR = join(REPO_PATH, "config")
+CONFIG_DIR = join(REPO_PATH, "configs")
 
 DATA_ROOT_DIR = "/scratche/users/sansiddh/abdomenCT-1k/"
