171bd26cf0c069d0f37abaef42b5c7dcb0b7bbdf
Sansiddh Jain <sansiddh@wadhwaniai.org>
2022-01-24 03:34:09 +0530

    Wrote v1 of dataloader, yet to debug

--- src/data/datasets/lidc_idri.py
+++ src/data/datasets/lidc_idri.py
@@ -12,6 +12,7 @@ import pylidc as pl
 import tqdm
 from PIL import Image, ImageDraw
 from pydicom import dcmread
+from scipy.sparse import csc_matrix
 from torch.utils.data import Dataset
 
 
@@ -27,10 +28,13 @@ class LIDC_IDRI(Dataset):
         Transformed image, label and image path through __getitem__() function.
     """
 
-    def __init__(self, transform=None, phase="train", sample=None, fraction=1.0, 
+    def __init__(self, ROOT='/scratche/users/sansiddh/LIDC-IDRI/data/',
+                 transform=None, phase="train", sample=None, fraction=1.0,
                  split_version="v3", task='semantic-segmentation', **kwargs):
 
-        # Loads images list
+        self.ROOT = ROOT
+        self.transform = transform
+        # Load split file
         if split_version is not None:
             self.splitfile_path = os.path.join(
                 self.ROOT, "splits", split_version, f"{phase}.txt"
@@ -38,14 +42,22 @@ class LIDC_IDRI(Dataset):
         else:
             self.splitfile_path = None
 
-        
+        # Get list of Scan objects based on splitfile
+        self.all_scans = self.get_list_of_scans(self.splitfile_path)
 
-        if phase == "train":
-            # TODO: Should this disabled by default?
-            # TODO: Check this implementation for bugs.
-            if sample is not None:
-                sampling_object = getattr(self, sample)
-                self.image_list = sampling_object(self)
+        # Creates mapping between image filename and image SOP ID
+        self.sop_id_to_fname_dict = self.create_imagename_sop_id_mapping()
+
+        # Get dataframe containing all masks and all imagepaths (main func)
+        self.image_mask_mapping = self.create_image_masks_mapping()
+
+        # Perform class wise upsampling/downsampling based on config
+        # if phase == "train":
+        #     if sample is not None:
+        #         sampling_object = getattr(self, sample)
+        #         self.image_list = sampling_object(self)
+        
+        # TODO : Get fractioning working for pandas dataframes 
 
     
     def convert_dicom_to_image(self, dicom, return_type='numpy'):
@@ -72,65 +84,121 @@ class LIDC_IDRI(Dataset):
         else:
             raise ValueError('`return_type` can be one of PIL or numpy')
 
-    def create_mask_from_contour(self, contour, width=512, height=512):
+    def create_mask_from_contour(self, contour, img_shape):
         coords = contour.to_matrix(include_k=False)
         coords = np.flip(coords, axis=1)
-        dummy_img = Image.new('L', (width, height), 0)
+        dummy_img = Image.new('L', img_shape, 0)
         ImageDraw.Draw(dummy_img).polygon(list(map(tuple, coords)), outline=1, fill=1)
         mask = np.array(dummy_img)
 
         return mask
 
+    def get_masks_for_single_sample(self, scan):
+        images = scan.load_all_dicom_images()
+        df_images_masks = pd.DataFrame(columns=['image', 'mask'])
+        for i, image in enumerate(images):
+            fname = self.sop_id_to_fname_dict[image.SOPInstanceUID]
+            df_images_masks.loc[i, 'image'] = fname
+            df_images_masks.loc[i, 'mask'] = csc_matrix(
+                np.zeros(image.pixel_array.shape))
+
+        all_annotations = scan.annotations
+        for annotation in all_annotations:
+            contours = annotation.contours
+            for contour in contours:
+                image = images[contour.image_k_position]
+                fname = self.sop_id_to_fname_dict[image.SOPInstanceUID]
+                mask = self.create_mask_from_contour(
+                    contour, image.pixel_array.shape)
+                sparse_mask = csc_matrix(mask)
+
+                idx = df_images_masks[df_images_masks['image'] == fname].index[0]
+                org_mask = df_images_masks.loc[idx, 'mask']
+                if len(org_mask.nonzero()[0]) == 0:
+                    df_images_masks.loc[idx, 'mask'] = sparse_mask
+                else:
+                    new_idx = len(df_images_masks)
+                    df_images_masks.loc[new_idx, 'image'] = fname
+                    df_images_masks.loc[new_idx, 'mask'] = sparse_mask
+
+        return df_images_masks
+
+    def create_image_masks_mapping(self):
+        df_master = self.get_masks_for_single_sample(self.all_scans[0])
+        for scan in self.all_scans[1:]:
+            df = self.get_masks_for_single_sample(scan)
+            df_master = pd.concat([df_master, df], ignore_index=True)
+
+        return df_master
 
 
-    def get_image_list(self, splitfile_path):
+    def create_imagename_sop_id_mapping(self):
+        try:
+            df = pd.read_csv('/scratche/users/sansiddh/LIDC-IDRI/fname_sop_id_mapping.csv')
+        except Exception:
+            print('This will take a long while...')
+            all_dicoms = glob(self.ROOT+'LIDC-IDRI-*/*/*/*.dcm')
+            df = pd.DataFrame(columns=['filename', 'sop_instance_id'])
+            for i, path in tqdm(enumerate(all_dicoms)):
+                df.loc[i, 'filename'] = path
+                dicom = dcmread(path)
+                df.loc[i, 'sop_instance_id'] = dicom.SOPInstanceUID
+            
+            df.to_csv('/scratche/users/sansiddh/LIDC-IDRI/fname_sop_id_mapping.csv')
+        
+        sop_id_to_fname_dict = dict(zip(df['sop_instance_id'], df['filename']))
+
+        return sop_id_to_fname_dict
+
+
+    def get_list_of_scans(self, splitfile_path):
         """Read images from splits file and shuffle
         """
         if splitfile_path is not None:
             with open(splitfile_path, "r") as f:
-                image_list = f.read().split("\n")
+                patient_list = f.read().split("\n")
+            all_scans = pl.query(pl.Scan).filter(
+                pl.Scan.patient_id.in_(patient_list)).all()
         else:
-            image_list = glob(f'{self.ROOT}Images/*/*.jpg')
+            all_scans = pl.query(pl.Scan).all()
 
-        random.shuffle(image_list)
-        print("> Found total images : {}".format(len(image_list)))
+        random.shuffle(all_scans)
+        print("> Total number of patients : {}".format(len(all_scans)))
 
-        return image_list
+        return all_scans
 
-    def downsampling_negatives(self):
-        """Assumes negatives are more than positives, and downsamples thme
-        """
+    # def downsampling_negatives(self):
+    #     """Assumes negatives are more than positives, and downsamples thme
+    #     """
 
-        print("Downsampling : negative set = positive set ...")
-        # ROUND2-TODO: consider a random.shuffle(mylist) followed by sub selection mylist[:self.pos_indices].
-        downsampled_neg_indices = random.sample(
-            self.neg_indices, len(self.pos_indices))
-        downsampled_image_list = list(
-            np.array(self.image_list)[
-                self.pos_indices + downsampled_neg_indices]
-        )
+    #     print("Downsampling : negative set = positive set ...")
+    #     # ROUND2-TODO: consider a random.shuffle(mylist) followed by sub selection mylist[:self.pos_indices].
+    #     downsampled_neg_indices = random.sample(
+    #         self.neg_indices, len(self.pos_indices))
+    #     downsampled_image_list = list(
+    #         np.array(self.image_list)[
+    #             self.pos_indices + downsampled_neg_indices]
+    #     )
 
-        return downsampled_image_list
+    #     return downsampled_image_list
 
-    def upsampling_positives(self):
-        """Assumes positives are fewer than negatives, and upsamples thme
-        """
+    # def upsampling_positives(self):
+    #     """Assumes positives are fewer than negatives, and upsamples thme
+    #     """
 
-        print("Upsampling : positive set = negative set ...")
-        upsample_len = len(self.neg_indices) - len(self.pos_indices)
-        upsampled_indices = random.choices(self.pos_indices, k=upsample_len)
-        upsampled_image_list = list(
-            np.array(self.image_list)[
-                self.pos_indices + self.neg_indices + upsampled_indices
-            ]
-        )
+    #     print("Upsampling : positive set = negative set ...")
+    #     upsample_len = len(self.neg_indices) - len(self.pos_indices)
+    #     upsampled_indices = random.choices(self.pos_indices, k=upsample_len)
+    #     upsampled_image_list = list(
+    #         np.array(self.image_list)[
+    #             self.pos_indices + self.neg_indices + upsampled_indices
+    #         ]
+    #     )
 
-        return upsampled_image_list
+    #     return upsampled_image_list
 
     def get_dataset_subset(self, fraction):
         num_samples = int(fraction * len(self.image_list))
-        # ROUND2-TODO: consider a random.shuffle(mylist) followed by sub selection mylist[:self.pos_indices].
-        # SJ : This is already happening, we do random.shuffle(list) when we load the images
         image_list = random.sample(self.image_list, num_samples)
 
         print("> No. total samples using: {}".format(len(image_list)))
@@ -140,16 +208,14 @@ class LIDC_IDRI(Dataset):
         return len(self.image_list)
 
     def __getitem__(self, idx):
-        image_path = self.image_list[idx]
-        mask_path = image_path.replace(
-            '.jpg', '.npy').replace('Images', 'Masks')
-        # image = self.load_and_process_image(image_path, self.transform)
-        image = Image.open(image_path)
-        mask = np.load(mask_path)
+        image_path = self.image_mask_mapping[idx, 'image']
+        dicom = dcmread(image_path)
+        image = self.convert_dicom_to_image(dicom, return_type='PIL')
+        mask = self.image_mask_mapping[idx, 'mask'].toarray()
+
         transformed = self.transform(image=image, mask=mask)
-        # print(self.transform)
+
         image = transformed['image']
         mask = transformed['mask']
 
-        print(image.shape)
         return image, mask
