d617c98139cbe60d850df56f6919d24f35c0cfc2
Sansiddh Jain <sansiddh@wadhwaniai.org>
2022-01-24 20:17:43 +0530

    Copied helper.py from ultrasound repo

--- src/main/helper.py
+++ src/main/helper.py
@@ -1,18 +1,19 @@
 import os
+import pickle
 import random
 import time
 from warnings import warn
 
 import numpy as np
+import pandas as pd
 import torch
 import wandb
 import yaml
-from natsort import natsorted
 
 import src.models as models_module
 import src.utils.metrics as metrics_module
 import src.viz.eval as viz_eval_module
-import src.utils.losses as losses_module
+from natsort import natsorted
 from src.data.datasets.main import create_dataloader
 
 
@@ -52,7 +53,7 @@ def setup_checkpoint_dir(cfg, args, phase):
     """
 
     root_dir = os.path.join(
-        cfg["checkpoints_dir"], cfg["model"], args.config_name
+        cfg["checkpoints_dir"], cfg["model"]["name"], args.config_name
     )
 
     ckpt_dir = os.path.join(root_dir, "checkpoints")
@@ -69,7 +70,7 @@ def load_checkpoints(cfg, args, model, optimizer, checkpoint_id="last"):
     print(f"> ***** Loading checkpoint from {args.config_name} *****")
 
     checkpoint_dir = os.path.join(
-        cfg["checkpoints_dir"], cfg["model"], args.config_name, "checkpoints"
+        cfg["checkpoints_dir"], cfg["model"]["name"], args.config_name, "checkpoints"
     )
 
     if checkpoint_id == "best":
@@ -112,14 +113,12 @@ def load_checkpoints(cfg, args, model, optimizer, checkpoint_id="last"):
 def init_wandb(cfg, args, root_dir, model, phase):
     # Create Wandb run name
     if phase == "inference":
-        wandb_run = "{}_{}_{}".format(
-            cfg["model"], args.config_name, phase)
+        wandb_run = "{}_{}_{}".format(cfg["model"]["name"], args.config_name, phase)
     else:
         if args.run:
             wandb_run = args.run
         else:
-            wandb_run = "{}_{}_{}".format(
-                cfg["model"], args.config_name, phase)
+            wandb_run = "{}_{}_{}".format(cfg["model"]["name"], args.config_name, phase)
     # Initialize
     if phase == "train":
         wandb.init(
@@ -154,9 +153,9 @@ def log_to_wandb(
     epochID=0,
 ):
     if phase not in ["train", "inference"]:
-        raise Exception(
-            "Invalid phase! Please choose one of ['train', 'inference']")
-    wandb.log(figures_dict, step=epochID)
+        raise Exception("Invalid phase! Please choose one of ['train', 'inference']")
+    new_fig_dict = convert_to_wandb_images(figures_dict)
+    wandb.log(new_fig_dict, step=epochID)
     if phase == "train":
         wandb.log(train_metrics, step=epochID)
         wandb.log(val_metrics, step=epochID)
@@ -180,18 +179,18 @@ def get_dataloader(cfg, phase):
 def initialise_objs(cfg, device, phase):
     print("Initializing the model...")
     tick = time.time()
-    # Assumption here that the models that will be called will be from the segmentation_models_pytorch module
-    model_class = getattr(models_module, cfg["model"])
-    model = model_class(**cfg["model_params"]).to(device)
+    model_class = getattr(models_module, cfg["model"]["name"])
+    model = model_class(
+        num_classes=cfg["data"]["dataset_params"]["NUM_CLASSES"],
+        pretrained=cfg["model"]["pretrained"],
+    ).to(device)
     print("> Time to initialize model : {:.4f} sec".format(time.time() - tick))
 
     # Create criterion object
-    criterion_class = getattr(losses_module, cfg[phase]["loss"])
-    if cfg[phase]["loss"] == 'CrossEntropyLoss':
-        weight = torch.tensor(cfg[phase]["loss_params"]["weight"])
-        criterion = criterion_class(weight=weight).to(device)
-    else:
-        criterion = criterion_class(**cfg[phase]["loss_params"]).to(device)
+    criterion_class = getattr(torch.nn, cfg[phase]["criterion"])
+
+    weight = torch.tensor(cfg[phase]["criterion_params"]["weight"])
+    criterion = criterion_class(pos_weight=weight).to(device)
 
     if phase == "train":
         # Create optimiser object
@@ -199,17 +198,59 @@ def initialise_objs(cfg, device, phase):
         optimizer = optimizer_class(
             model.parameters(), **cfg[phase]["optimizer_params"]
         )
+        scheduler_class = getattr(torch.optim.lr_scheduler, cfg[phase]["lr_scheduler"])
+        scheduler = scheduler_class(optimizer, **cfg[phase]["lr_scheduler_params"])
     else:
         optimizer = None
+        scheduler = None
 
-    return model, criterion, optimizer
+    return model, criterion, optimizer, scheduler
 
 
-def evaluate(gt, preds, metric_type, cfg, metrics_dict, phase):
+def evaluate(gt, preds, metric_type, cfg, phase):
+    return_dict = {}
     for metric in cfg["eval"]["logging_metrics"]["{}_metrics".format(metric_type)]:
         callable_metric = getattr(metrics_module, metric)
-        metrics_dict["{}_{}".format(phase, metric)
-                     ] = callable_metric(gt, preds)
+        return_dict["{}_{}".format(phase, metric)] = callable_metric(gt, preds)
+
+    return return_dict
+
+
+def calculate_metrics(cfg, gt_labels, pred_scores, pred_labels, phase, dataloader):
+    metrics_dict = {}
+    if (
+        cfg["task_type"] == "binary-classification"
+        or cfg["task_type"] == "multiclass-classification"
+    ):
+        # TODO: Make scores implementation compatible for multiple classes
+        metrics_dict.update(evaluate(gt_labels, pred_scores, "score", cfg, phase))
+        metrics_dict.update(evaluate(gt_labels, pred_labels, "label", cfg, phase))
+    elif cfg["task_type"] == "multilabel-classification":
+        name_to_code_mapping = dataloader.dataset.name_to_feature_code_mapping
+        code_to_name_mapping = {v: k for k, v in name_to_code_mapping.items()}
+        df_dict = {}
+        for i in range(gt_labels.shape[1]):
+            feature_name = code_to_name_mapping[i]
+            feature_dict = evaluate(
+                gt_labels[:, i], pred_scores[:, i], "score", cfg, phase
+            )
+            feature_dict.update(
+                evaluate(gt_labels[:, i], pred_labels[:, i], "label", cfg, phase)
+            )
+            df_dict[feature_name] = feature_dict
+            feature_dict = {f"{feature_name}_{k}": v for k, v in feature_dict.items()}
+            metrics_dict.update(feature_dict)
+
+        metrics_df = pd.DataFrame.from_dict(df_dict).T
+        metrics_df.reset_index(inplace=True)
+        metrics_dict.update(
+            {phase + "_summary_table": wandb.Table(dataframe=metrics_df)}
+        )
+    else:
+        raise ValueError(
+            "Support for given task_type is not yet present in the metrics module."
+            + "Please choose one of - `binary-classification`, `multiclass-classification`, or `multilabel-classification`"
+        )
 
     return metrics_dict
 
@@ -223,6 +264,7 @@ def create_figures(
     val_scores=None,
     test_gt_labels=None,
     test_scores=None,
+    labels_dict=None,
 ):
     """
     Plot figures to be logged to wandb.
@@ -246,19 +288,39 @@ def create_figures(
         dict: A dictionary containing mappings to all figures that need to be plotted according to the config file
     """
     if phase not in ["train", "inference"]:
-        raise Exception(
-            "Invalid phase! Please choose one of ['train', 'inference']")
+        raise Exception("Invalid phase! Please choose one of ['train', 'inference']")
     figures_dict = {}
+
     if phase == "train":
-        y_true_arr = [train_gt_labels, val_gt_labels]
-        y_pred_proba_arr = [train_scores, val_scores]
-        labels_arr = ["Train", "Val"]
-        plot_thres_for_idx = 1
+        if cfg["task_type"] == "binary-classification":
+            y_true_arr = [train_gt_labels, val_gt_labels]
+            y_pred_proba_arr = [train_scores, val_scores]
+            labels_arr = ["Train", "Val"]
+            plot_thres_for_idx = [1]
+        elif cfg["task_type"] == "multilabel-classification":
+            y_true_arr, y_pred_proba_arr, labels_arr, plot_thres_for_idx = [
+                [] for i in range(4)
+            ]
+            for i in range(train_gt_labels.shape[1]):
+                y_true_arr.append(train_gt_labels[:, i])
+                y_pred_proba_arr.append(train_scores[:, i])
+                labels_arr.append(labels_dict[i])
+                plot_thres_for_idx.append(i)
     else:
-        y_true_arr = [test_gt_labels]
-        y_pred_proba_arr = [test_scores]
-        labels_arr = ["Test"]
-        plot_thres_for_idx = 0
+        if cfg["task_type"] == "binary-classification":
+            y_true_arr = [test_gt_labels]
+            y_pred_proba_arr = [test_scores]
+            labels_arr = ["Test"]
+            plot_thres_for_idx = [0]
+        elif cfg["task_type"] == "multilabel-classification":
+            y_true_arr, y_pred_proba_arr, labels_arr, plot_thres_for_idx = [
+                [] for i in range(4)
+            ]
+            for i in range(test_gt_labels.shape[1]):
+                y_true_arr.append(test_gt_labels[:, i])
+                y_pred_proba_arr.append(test_scores[:, i])
+                labels_arr.append(labels_dict[i])
+                plot_thres_for_idx.append(i)
     for func in cfg["viz"]["eval"]:
         if func == "plot_pr_curve":
             fig, _ = getattr(viz_eval_module, func)(
@@ -267,7 +329,7 @@ def create_figures(
                 labels_arr,
                 plot_thres_for_idx=plot_thres_for_idx,
                 pos_label=1,
-                plot_prevalance_for_idx=0,
+                plot_prevalance_for_idx=plot_thres_for_idx,
             )
         else:
             fig, _ = getattr(viz_eval_module, func)(
@@ -277,9 +339,17 @@ def create_figures(
                 plot_thres_for_idx=plot_thres_for_idx,
                 pos_label=1,
             )
-        figures_dict[func] = [wandb.Image(fig)]
+        figures_dict[func] = fig
 
     return figures_dict
+
+
+def convert_to_wandb_images(figures_dict):
+    new_fig_dict = dict()
+    for fig in figures_dict:
+        new_fig_dict[fig] = [wandb.Image(figures_dict[fig])]
+
+    return new_fig_dict
 
 
 def epoch(cfg, model, dataloader, criterion, optimizer, device, phase, scaler):
@@ -319,20 +389,20 @@ def epoch(cfg, model, dataloader, criterion, optimizer, device, phase, scaler):
     total = len(dataloader)
 
     tick = time.time()
-    for batchID, (images, masks) in enumerate(dataloader):
+    for batchID, (images, labels, image_path) in enumerate(dataloader):
         tock = time.time()
+        labels = labels.float()
         labels = labels.to(device)
 
         with torch.cuda.amp.autocast(enabled=cfg[phase]["use_amp"]):
             with torch.set_grad_enabled(phase == "train"):
                 output = model(images.to(device))
-                import pdb; pdb.set_trace()
                 if (
-                    cfg["model"] == "inception"
-                    or cfg["model"] == "googlenet"
+                    cfg["model"]["name"] == "inception"
+                    or cfg["model"]["name"] == "googlenet"
                 ):
                     output = output.logits
-                loss = criterion(output, masks)
+                loss = criterion(output, labels)
 
         current = batchID + 1
         percent = current / float(total)
@@ -351,13 +421,12 @@ def epoch(cfg, model, dataloader, criterion, optimizer, device, phase, scaler):
         # Append GT Labels and Pred Scores
         gt_labels += list(labels.cpu().float().numpy())
         if phase == "train":
-            scores = (torch.softmax(output, -1)).detach().cpu().numpy()
+            scores = torch.sigmoid(output).detach().cpu().numpy()
         else:
-            scores = (torch.softmax(output, -1)).cpu().numpy()
+            scores = torch.sigmoid(output).cpu().numpy()
         pred_scores += scores.tolist()
 
-        pred_label = torch.argmax(torch.softmax(output, -1), -1).cpu().numpy()
-        pred_labels += pred_label.tolist()
+        pred_labels += (scores > 0.5).astype(float).tolist()
 
         if phase == "train":
             # Setting grad=0 using another method instead of optimizer.zero_grad()
@@ -373,20 +442,17 @@ def epoch(cfg, model, dataloader, criterion, optimizer, device, phase, scaler):
                 optimizer.step()
         tick = time.time()
 
+        image_paths.extend(image_path)
+
     average_loss = np.mean(losses)
     average_time = np.mean(batch_times)
     gt_labels = np.array(gt_labels)
     pred_scores = np.array(pred_scores)
     pred_labels = np.array(pred_labels)
 
-    metrics_dict = dict()
-
-    # TODO: Make scores implementation compatible for multiple classes
-    metrics_dict = evaluate(
-        gt_labels, pred_scores[:, 1], "score", cfg, metrics_dict, phase
+    metrics_dict = calculate_metrics(
+        cfg, gt_labels, pred_scores, pred_labels, phase, dataloader
     )
-    metrics_dict = evaluate(gt_labels, pred_labels,
-                            "label", cfg, metrics_dict, phase)
 
     bar = "[" + symbol * width + "]"
     print(
@@ -395,7 +461,7 @@ def epoch(cfg, model, dataloader, criterion, optimizer, device, phase, scaler):
         )
     )
 
-    return average_loss, metrics_dict, gt_labels, pred_scores, pred_labels
+    return average_loss, metrics_dict, gt_labels, pred_scores, pred_labels, image_paths
 
 
 def save_model_checkpoints(
@@ -408,7 +474,7 @@ def save_model_checkpoints(
         "metric": ckpt_metric,
     }
 
-    metric_key = "{}_{}".format(phase, cfg["eval"]["ckpt_metric"])
+    metric_key = cfg["eval"]["ckpt_metric"]
     if metrics[metric_key] > ckpt_metric:
         ckpt_metric = metrics[metric_key]
         best_model_path = os.path.join(ckpt_dir, "best_model.pth.tar")
@@ -417,3 +483,26 @@ def save_model_checkpoints(
     model_name = "checkpoint-{}.pth.tar".format(epochID)
     model_path = os.path.join(ckpt_dir, model_name)
     torch.save(state_dict, model_path)
+
+
+def parse_image_path(path):
+    # Round2-TODO: Check if the path is consistent with ms1-data
+
+    path_items = path.split("/")
+    dataset = path_items[-7]
+    data = path_items[-6]
+    class_label = path_items[-5]
+    patient = path_items[-4]
+    video = path_items[-3]
+    annotator_task = path_items[-2]
+    image_num = path_items[-1]
+
+    return dataset, data, class_label, patient, video, annotator_task, image_num
+
+
+def get_cached_dict(filepath):
+    f = open(filepath, "rb")
+    saved_obj = pickle.load(f)
+    cached_dict, figures, metrics = saved_obj
+
+    return cached_dict, figures, metrics
