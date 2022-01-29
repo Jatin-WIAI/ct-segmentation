449bb3c851e9520878febe3535fc557d21f3f865
Sansiddh Jain <sansiddh@wadhwaniai.org>
2022-01-29 16:36:58 +0530

    Explicitly importing all smp losses

--- src/utils/losses.py
+++ src/utils/losses.py
@@ -1,4 +1,9 @@
-from segmentation_models_pytorch.losses import *
+from segmentation_models_pytorch.losses import (DiceLoss, FocalLoss,
+                                                JaccardLoss, LovaszLoss,
+                                                MCCLoss, SoftBCEWithLogitsLoss,
+                                                SoftCrossEntropyLoss,
+                                                TverskyLoss)
+# Read more here - https://smp.readthedocs.io/en/latest/losses.html
 from torch.nn import (BCELoss, BCEWithLogitsLoss, CosineEmbeddingLoss,
                       CrossEntropyLoss, CTCLoss, GaussianNLLLoss,
                       HingeEmbeddingLoss, HuberLoss, KLDivLoss, L1Loss,
@@ -6,3 +11,4 @@ from torch.nn import (BCELoss, BCEWithLogitsLoss, CosineEmbeddingLoss,
                       MultiLabelSoftMarginLoss, MultiMarginLoss, NLLLoss,
                       PoissonNLLLoss, SmoothL1Loss, SoftMarginLoss,
                       TripletMarginLoss, TripletMarginWithDistanceLoss)
+# Read more here - https://pytorch.org/docs/stable/nn.html#loss-functions
