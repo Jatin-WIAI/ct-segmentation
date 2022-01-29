47fce82d5c6bf53583e245ea988779f01a1dd6ac
Sansiddh Jain <sansiddh@wadhwaniai.org>
2022-01-29 16:37:25 +0530

    Added all monai losses

--- src/utils/metrics.py
+++ src/utils/metrics.py
@@ -2,9 +2,16 @@
 """
 
 import numpy as np
-from sklearn.metrics import *
-from keras import backend as K
-import surface_distance
+from sklearn.metrics import (accuracy_score, average_precision_score,
+                             brier_score_loss, classification_report,
+                             cohen_kappa_score, confusion_matrix, det_curve,
+                             f1_score, fbeta_score, hamming_loss, hinge_loss,
+                             jaccard_score, log_loss, matthews_corrcoef,
+                             plot_det_curve, precision_recall_curve,
+                             precision_score, recall_score, roc_auc_score)
+
+import segmentation_models_pytorch as smp
+smp.metrics.get_stats
 
 
 def precision_at_90recall(gt_labels, pred_scores):
