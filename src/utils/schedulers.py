bb01d08c44ef1ed8cd5f310bb31585c40022017b
Sansiddh Jain <sansiddh@wadhwaniai.org>
2022-02-01 16:23:28 +0530

    Added docstrings

--- src/utils/schedulers.py
+++ src/utils/schedulers.py
@@ -1,1 +1,10 @@
-from torch.optim.lr_scheduler import *
+"""All Learning Rate Schedulers. Currently only support torch LR Schedulers.
+"""
+
+from torch.optim.lr_scheduler import (ChainedScheduler, ConstantLR,
+                                      CosineAnnealingLR,
+                                      CosineAnnealingWarmRestarts, CyclicLR,
+                                      ExponentialLR, LambdaLR, LinearLR,
+                                      MultiplicativeLR, MultiStepLR,
+                                      OneCycleLR, ReduceLROnPlateau,
+                                      SequentialLR, StepLR)
